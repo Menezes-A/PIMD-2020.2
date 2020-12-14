a8cdd27bb4d7a72da41618d77398d9f863e10b91
Menezes-A <analivia.fm4@gmail.com>
2020-12-14 10:36:07 -0300

    Atividade 2

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Cabeçalho from './componentes/Cabeçalho';
-import Corpo from './componentes/Corpo';
-import Disciplina from './componentes/Disciplina';
+import { View } from 'react-native';
+import Home from './Atividade 2/Home';
+import Navbar from './Atividade 2/Navbar';
 
 export default function App() {
   return (
     <View>
-      <Cabeçalho nome="Raul" curso="Engenharia Civil"></Cabeçalho>
-
-      <Disciplina disciplina="Calculo I"></Disciplina>
-
-      <Disciplina disciplina="Calculo II"></Disciplina>
-
-      <Corpo></Corpo>
+      <StatusBar backgroundColor={"#381859"}></StatusBar>
+      <Navbar></Navbar>
+      <Home></Home>
     </View>
   );
 }
